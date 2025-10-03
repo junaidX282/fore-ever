892e245fb862ee87399b39ee6814a85f1b43963b
junaid <junaid.vjunu9895@gmail.com>
2025-10-03 15:24:40 +0530

    Fix component import paths for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import SearchBar from './components/searchBar'
+import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import Contact from './pages/Contact'
 
